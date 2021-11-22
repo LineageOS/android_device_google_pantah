9884503f11002676573d90f712b8e8861d153888
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Lineage product makefiles
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

---
+++ lineage_panther.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/pantah/aosp_panther.mk)
+$(call inherit-product, device/google/gs201/lineage_common.mk)
+$(call inherit-product, device/google/pantah/device-lineage.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 7
+PRODUCT_NAME := lineage_panther
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=panther \
+    PRIVATE_BUILD_DESC="panther-user 13 TD1A.220804.031 9071314 release-keys"
+
+BUILD_FINGERPRINT := google/panther/panther:13/TD1A.220804.031/9071314:user/release-keys
---
+++ panther/BoardConfigLineage.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include device/google/pantah/BoardConfigLineage.mk
