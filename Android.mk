7f1e1bcfd51990a4fb4edba198a3b5b8dec1aa0b
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-04 01:01:42 +0530

    pantah: Set up proprietary files extraction
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I17b78ccf212d8364070c40fe3c7d06d4048e3fe4

--- Android.mk
+++ Android.mk
@@ -27,4 +27,29 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq (,$(filter $(TARGET_DEVICE),cloudripper ravenclaw cheetah panther))
   include $(call all-makefiles-under,$(LOCAL_PATH))
+
+DM_LIBS := libdmengine.so libdmjavaplugin.so
+DM_32_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT)/priv-app/DMService/lib/arm/,$(notdir $(DM_LIBS)))
+$(DM_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "DMService 32 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /product/lib/$(notdir $@) $@
+
+DM_64_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT)/priv-app/DMService/lib/arm64/,$(notdir $(DM_LIBS)))
+$(DM_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "DMService 64 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /product/lib64/$(notdir $@) $@
+
+HBM_LIBS := libhbmsvmanager_jni.so
+HBM_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT)/priv-app/HbmSVManager/lib/arm64/,$(notdir $(HBM_LIBS)))
+$(HBM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "HBM lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system_ext/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(DM_32_SYMLINKS) $(DM_64_SYMLINKS) $(HBM_SYMLINKS)
 endif
--- lineage_cheetah.mk
+++ lineage_cheetah.mk
@@ -27,3 +27,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="cheetah-user 14 UP1A.231005.007 10754064 release-keys"
 
 BUILD_FINGERPRINT := google/cheetah/cheetah:14/UP1A.231005.007/10754064:user/release-keys
+
+$(call inherit-product, vendor/google/cheetah/cheetah-vendor.mk)
