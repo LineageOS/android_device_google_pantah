caa61631bda6aef5237cd4e0ea5d0c97053cced9
Michael Bestas <mkbestas@lineageos.org>
2022-06-10 05:14:06 +0300

    pantah: Inline kernel building
    Change-Id: I629c70b74db44d02847212b774982060ab84f1d2

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -5,3 +5,17 @@
 #
 
 BUILD_BROKEN_DUP_RULES := true
+
+# Kernel
+TARGET_KERNEL_DTBO_PREFIX := dts/
+TARGET_KERNEL_DTBO := google/dtbo.img
+TARGET_KERNEL_DTB := \
+    google/gs201-a0.dtb \
+    google/gs201-b0.dtb
+
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat device/google/pantah/vendor_dlkm.modules.load))
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(foreach m,$(BOARD_VENDOR_KERNEL_MODULES_LOAD_RAW),$(notdir $(m)))
+BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat device/google/pantah/vendor_kernel_boot.modules.load))
+BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD := $(foreach m,$(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW),$(notdir $(m)))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD)
