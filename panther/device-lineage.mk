0ff52d86a11fc4d26d6448064072a327a3f3cfb4
Michael Bestas <mkbestas@lineageos.org>
2022-08-13 04:25:09 +0300

    pantah: Copy touch modules to vendor_kernel_boot
    Makes touch work in recovery
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: Iaa5a158af827732b13ab79b60bed2f319ce56391

--- panther/device-lineage.mk
+++ panther/device-lineage.mk
@@ -11,3 +11,7 @@ DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/$(DEVICE_CODENAME)/overlay-lineage
 # HBM
 PRODUCT_PACKAGES += \
     HbmSVManagerOverlayPanther
+
+# Init
+PRODUCT_PACKAGES += \
+    init.recovery.panther.touch.rc
