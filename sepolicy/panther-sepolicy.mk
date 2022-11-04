218212ae6c0cb7dbbb0e545f6363a9597c3b3bd1
Michael Bestas <mkbestas@lineageos.org>
2022-11-04 03:28:51 +0200

    Update sepolicy path
    Change-Id: Ia24ef37000ed9e1cdf994a32447b0250e3f89186

--- sepolicy/panther-sepolicy.mk
+++ sepolicy/panther-sepolicy.mk
@@ -1,2 +1,2 @@
 # sepolicy that are shared among devices using whitechapel
-BOARD_SEPOLICY_DIRS += device/google/pantah-sepolicy/panther
+BOARD_SEPOLICY_DIRS += device/google/pantah/sepolicy/panther
