c120565568f528131c2a7f5a01bead3f30688032
Michael Bestas <mkbestas@lineageos.org>
2023-01-04 01:40:41 +0200

    [SQUASH] Merge tag 'android-13.0.0_r23' into staging/lineage-20_merge-android-13.0.0_r23
    Change-Id: I7ff5fe616666b83729a52bb90257abd4ae4db8f3

--- device-panther.mk
+++ device-panther.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES_DEBUG += \
     hci_inject
 endif
 
+# Bluetooth OPUS codec
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.bluetooth.opus.enabled=true
+
 # Bluetooth Tx power caps
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bluetooth_power_limits_panther.csv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_power_limits.csv \
@@ -162,9 +166,9 @@ PRODUCT_PACKAGES_DEBUG += \
     sar_test
 endif
 
-# declare use of spatial audio: disabled
+# declare use of spatial audio
 PRODUCT_PROPERTY_OVERRIDES += \
-       ro.audio.spatializer_enabled=false
+       ro.audio.spatializer_enabled=true
 
 # Keymaster HAL
 #LOCAL_KEYMASTER_PRODUCT_PACKAGE ?= android.hardware.keymaster@4.1-service
@@ -250,7 +254,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=9
+    ro.vendor.build.svn=11
 
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \
