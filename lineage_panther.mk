2b93865e4cc45b13b487758235d549652731bb48
Michael Bestas <mkbestas@lineageos.org>
2023-01-05 17:52:32 +0200

    Update fingerprint/build description from TQ1A.230105.002
    Change-Id: I741ef8870423142e2745b7b61a511ff6fe2f21f9

--- lineage_panther.mk
+++ lineage_panther.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=panther \
-    PRIVATE_BUILD_DESC="panther-user 13 TQ1A.221205.011 9244662 release-keys"
+    PRIVATE_BUILD_DESC="panther-user 13 TQ1A.230105.001.A2 9325585 release-keys"
 
-BUILD_FINGERPRINT := google/panther/panther:13/TQ1A.221205.011/9244662:user/release-keys
+BUILD_FINGERPRINT := google/panther/panther:13/TQ1A.230105.001.A2/9325585:user/release-keys
 
 $(call inherit-product, vendor/google/panther/panther-vendor.mk)
