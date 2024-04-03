604ba5428ad478db388a68e3e3909551de2a1fb9
Ankur Bakshi <ankurbakshi@google.com>
2024-04-03 23:06:03 +0000

    Update panther SVN to 56
    Bug: 330382848
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:6cbb1eb370842cd354f141162edc6f96d7558ac7)
    Merged-In: I98175f825b431119d5e5d60e878946e7da2ddacc
    Change-Id: I98175f825b431119d5e5d60e878946e7da2ddacc

--- device-panther.mk
+++ device-panther.mk
@@ -320,7 +320,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=55
+    ro.vendor.build.svn=56
 
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \
