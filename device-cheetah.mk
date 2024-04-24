b843fb4c09cb1129b3ff0c2de96fe2bc3e5c70cb
Ankur Bakshi <ankurbakshi@google.com>
2024-04-24 23:11:11 +0000

    Update cheetah SVN to 57
    Bug: 336526332
    Change-Id: Ib3b9baf24d3f0d5622ae63f11aa564df35fd14df

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -319,7 +319,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=56
+    ro.vendor.build.svn=57
 
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \
