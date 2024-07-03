0c979299eba911f04c4ab25ee34bc166342d3ef0
Michael Bestas <mkbestas@lineageos.org>
2024-07-03 18:18:57 +0300

    Update fingerprint/build description from AP2A.240705.004
    Change-Id: I10b1bc358c345c73c47eb00c8f74c988ddb575a4

--- lineage_panther.mk
+++ lineage_panther.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=panther \
-    PRIVATE_BUILD_DESC="panther-user 14 AP2A.240605.024 11860263 release-keys"
+    PRIVATE_BUILD_DESC="panther-user 14 AP2A.240705.004 11875680 release-keys"
 
-BUILD_FINGERPRINT := google/panther/panther:14/AP2A.240605.024/11860263:user/release-keys
+BUILD_FINGERPRINT := google/panther/panther:14/AP2A.240705.004/11875680:user/release-keys
 
 $(call inherit-product, vendor/google/panther/panther-vendor.mk)
