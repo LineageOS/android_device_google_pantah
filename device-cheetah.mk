dde3835959d6de20204a325eb24336f154ba37be
Michael Bestas <mkbestas@gmail.com>
2024-12-07 16:11:39 +0200

    Revert "BT: set system property for Bluetooth Auto On"
    This reverts commit 24776dbb422c32243ac41f76bbd24b40a81ee4bd.
    
    Issue: calyxos#2767
    Change-Id: I7391256dc2afa2773b53a5b5faf18ed093c399b6

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -50,10 +50,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
 	frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
-# Enable Bluetooth AutoOn feature
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.server.automatic_turn_on=true
-
 # Bluetooth OPUS codec
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.opus.enabled=true
