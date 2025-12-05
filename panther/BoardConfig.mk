525899405fbc1d38beb4955f311bf10a6cc34c1e
Michael Bestas <mkbestas@lineageos.org>
2025-12-05 09:17:34 +0200

    pantah: Commonize BoardConfig configuration
    Change-Id: I69975c928a0d5bb74dfc9e0e5685d5319d1048b0

--- panther/BoardConfig.mk
+++ panther/BoardConfig.mk
@@ -5,33 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Enable load module in parallel
-BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
-
-# The modules which need to be loaded in sequential
-BOARD_KERNEL_CMDLINE += fips140.load_sequential=1
-BOARD_KERNEL_CMDLINE += exynos_drm.load_sequential=1
-
-TARGET_BOARD_INFO_FILE := device/google/pantah/board-info.txt
-TARGET_BOOTLOADER_BOARD_NAME := panther
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := $(DEVICE_CODENAME)
 TARGET_SCREEN_DENSITY := 420
-
-BOARD_KERNEL_CMDLINE += swiotlb=noforce
-
-include device/google/gs201/BoardConfig-common.mk
-include device/google/pantah/wifi/BoardConfig-wifi.mk
-
-# Kernel modules
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/recovery/modules.blocklist.vendor_kernel_boot
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/recovery/modules.load.vendor_kernel_boot))
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)))
 
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/google/pantah/sepolicy/panther/vendor \
-    device/google/pantah/sepolicy/vendor \
-    hardware/google/pixel-sepolicy/vibrator/common \
-    hardware/google/pixel-sepolicy/vibrator/cs40l26
+    $(DEVICE_PATH)/sepolicy/$(DEVICE_CODENAME)/vendor
+
+include $(DEVICE_PATH)/BoardConfigCommon.mk
 
 include $(VENDOR_PATH)/BoardConfigVendor.mk
